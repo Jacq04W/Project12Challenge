a7dd58aa9f807e187e8d6ddbb65a4441bdbc6121
Jacquese Whitson <jacqwhitsonappledev@gmail.com>
2023-12-31 03:15:25 -0500

    Initial Commit

---
+++ Project12Challenge/Project12ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project12ChallengeApp.swift
+//  Project12Challenge
+//
+//  Created by Jacquese Whitson on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Project12ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
